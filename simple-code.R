967426db7c08aa79ae26e1bf3a12a95cc4a88313
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-03-23 16:22:28 +0100

    Add simple code file

---
+++ simple-code.R
@@ -0,0 +1,13 @@
+# This is an R script example file
+# It's here to demonstrate purpose of GitHub
+# Do reproduce this at home ;)
+
+library("ggplot2")
+
+print("Hello")
+
+head(mtcars)
+
+mtcars$cyl
+
+summary(mtcars)
